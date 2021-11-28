1aa6b217308700cedb46976602d07b8c02d7cca0
molla2021 <moget994@gmail.com>
2021-11-28 13:07:34 +0300

    api-tutorial useeffect fetch data commit commit

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,26 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
+import { getList } from '../../services/list';
 
 function App() {
   const [list, setList] = useState([]);
+  useEffect(() => {
+    let mounted = true;
+    getList()
+      .then(items => {
+        if(mounted) {
+          setList(items)
+        }
+      })
+    return () => mounted = false;
+  }, [])
   return(
-    <>
-    </>
+    <div className="wrapper">
+     <h1>My Grocery List</h1>
+     <ul>
+       {list.map(item => <li key={item.item}>{item.item}</li>)}
+     </ul>
+   </div>
   )
 }
 
